21eaa93e58240274982c43fddb4af2d31f988644
Martin Pettersson <martin@rememberberry.com>
2018-04-21 20:17:05 +0200

    Fix import error for newer pip versions

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import re
 from setuptools import setup
-from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 
 version = re.search('^__version__\s*=\s*"(.*)"',
